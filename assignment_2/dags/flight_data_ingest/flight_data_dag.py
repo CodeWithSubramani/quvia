b726a646bd4e5dad924b61ed78cf6beac16abc4e
Subramani R <subramani0595@gmail.com>
2025-04-12 17:29:41 +0530

    setup notifications

--- assignment_2/dags/flight_data_ingest/flight_data_dag.py
+++ assignment_2/dags/flight_data_ingest/flight_data_dag.py
@@ -2,7 +2,9 @@ import subprocess
 from datetime import datetime, timedelta
 
 from airflow import DAG
+from airflow.operators.email import EmailOperator
 from airflow.operators.python import PythonOperator
+from airflow.utils.email import send_email
 
 default_args = {
     'owner': 'airflow',
@@ -12,8 +14,7 @@ default_args = {
 
 
 def check_and_restart():
-    container_name = "flight-producer"
-    # Check if container is running
+    container_name = "assignment_2-flight-producer-1"
     result = subprocess.run(
         ["docker", "inspect", "-f", "{{.State.Running}}", container_name],
         stdout=subprocess.PIPE,
@@ -24,6 +25,12 @@ def check_and_restart():
     if status != "true":
         print(f"{container_name} is not running. Restarting...")
         subprocess.run(["docker", "restart", container_name])
+
+        send_email(
+            to=["subramani0595@gmail.com"],  # Replace with your email
+            subject="Flight Producer Restarted",
+            html_content=f"{container_name} was down and has been restarted by Airflow."
+        )
     else:
         print(f"{container_name} is running.")
 
@@ -32,10 +39,19 @@ with DAG(
         dag_id='monitor_flight_producer',
         default_args=default_args,
         start_date=datetime(2024, 1, 1),
-        schedule_interval='*/2 * * * *',  # Run every 2 minutes
+        schedule_interval='*/1 * * * *',  # Run every 2 minutes
         catchup=False
 ) as dag:
+    notify_start = EmailOperator(
+        task_id='notify_dag_start',
+        to='subramani0595@gmail.com',  # Replace with your email
+        subject='Flight Producer DAG Started',
+        html_content='The Flight Producer DAG has started.'
+    )
+
     monitor_task = PythonOperator(
         task_id='check_and_restart_flight_producer',
         python_callable=check_and_restart
     )
+
+monitor_task
